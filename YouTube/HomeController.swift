253591cd0cf9bb26af4f47d1c8f0ed7dd74a14e8
mitul jindal <mituljindal@gmail.com>
2017-10-22 22:10:17 +0530

    Home Screen Layout set

---
+++ YouTube/HomeController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by mitul jindal on 22/10/17.
+//  Copyright © 2017 mitul jindal. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = .white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
